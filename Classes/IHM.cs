eb37d93a11c1a7ba14a5ea1df1f7548cd651af46
Titouan Massé <titouanmasselaude@gmail.com>
2024-10-17 16:57:17 +0200

    ihm finis

--- Classes/IHM.cs
+++ Classes/IHM.cs
@@ -1,60 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Numerics;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExerciceFinaleCs.Classes
 {
-    public interface IHM
+    public class IHM
     {
-        public int etapeSaisie { get; set; }
-        public Dictionary<int, Client> listeClients { get; }
-        public Dictionary<int, Evenement> listeEvenements { get; }
+        public int etapeSaisie { get; set; } = 0;
+        public Dictionary<int, Client> listeClients { get; } = new Dictionary<int, Client>();
+        public Dictionary<int, Evenement> listeEvenements { get; } = new Dictionary<int, Evenement>();
+        public Client clientConnecter { get; set; }
 
+        public IHM()
+        {
+        }
 
         public void Start()
         {
-
-            //On génère des événements
+            // On génère des événements
             ajouterEvenement(new Evenement("Titanic", new Lieu("Nationale", "Lille", 120), "20/12/2024"));
             ajouterEvenement(new Evenement("Terrifier 3", new Lieu("Gambetta", "Lille", 40), "01/12/2024"));
             ajouterEvenement(new Evenement("Titanic", new Lieu("Bvrd de la Liberté", "Paris", 160), "20/12/2024"));
 
-
-
+            do
+            {
+                switch (etapeSaisie)
+                {
+                    case 0:
+                        introduction();
+                        break;
+                    case 1:
+                        creerClient();
+                        break;
+                    case 2:
+                        dejaClient();
+                        break;
+                    case 3:
+                        choisirEvenement();
+                        break;
+                }
+            }
+            while (true);
         }
-
 
         public void ajouterClient(Client client)
         {
             listeClients[listeClients.Count + 1] = client;
-            return;
         }
 
         public void ajouterEvenement(Evenement evenement)
         {
             listeEvenements[listeEvenements.Count + 1] = evenement;
-            return;
         }
 
-
-        public int introduction()
+        public void introduction()
         {
             Console.WriteLine("Bonjour, êtes-vous déjà enregistré ?");
             Console.WriteLine("oui\nnon");
             string choix = Console.ReadLine();
-            if(choix == "oui")
+            if (choix == "oui")
             {
-                return 1;
-            }else if(choix == "non")
-            {
-                return 2;
+                etapeSaisie = 2;
             }
-            return 0;
+            else if (choix == "non")
+            {
+                etapeSaisie = 1;
+            }
+        }
+
+        public void dejaClient()
+        {
+            Console.WriteLine("Voici les clients qui ont déjà acheté un billet chez nous : ");
+            // Affichage de tous les clients
+            foreach (var client in listeClients.Values)
+            {
+                Console.WriteLine($"- {client.NomPrenom}");
+            }
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Tapez le nom et le prénom exactement comme ils sont affichés pour pouvoir vous connecter. (tapez retour pour faire revenir en arrière)");
+            string reponse = Console.ReadLine();
+            if (reponse == "retour")
+            {
+                etapeSaisie = 0;
+                return;
+            }
+            else
+            {
+                foreach (var client in listeClients.Values)
+                {
+                    if (client.NomPrenom == reponse)
+                    {
+                        clientConnecter = client;
+                        Console.WriteLine($"Ravi de vous revoir {clientConnecter.NomPrenom}");
+                        etapeSaisie = 3; // Passer à la réservation après connexion
+                        return;
+                    }
+                }
+                // Si le client n'existe pas
+                Console.WriteLine("Le nom n'est pas valide /!\\");
+                etapeSaisie = 0;
+            }
         }
 
         public void creerClient()
@@ -86,20 +130,49 @@ namespace ExerciceFinaleCs.Classes
 
             ajouterClient(new Client(nomClient, prenomClient, new Adresse(rueClient, villeClient), ageClient, telClient));
             Console.WriteLine($"Bienvenue chez nous {prenomClient} !");
-            return;
+            clientConnecter = new Client(nomClient, prenomClient, new Adresse(rueClient, villeClient), ageClient, telClient);
+            etapeSaisie = 3; // Passer à la réservation après création du client
+        }
+
+        public void choisirEvenement()
+        {
+            Console.WriteLine("Voici la liste des événements disponibles : ");
+            foreach (var evenement in listeEvenements.Values)
+            {
+                Console.WriteLine($"- {evenement.Nom} à {evenement.Lieu.Ville}, le {evenement.Date}");
+            }
+            Console.WriteLine("Tapez le nom de l'événement auquel vous souhaitez assister (tapez retour pour revenir en arrière) :");
+            string nomEvenement = Console.ReadLine();
+            if (nomEvenement == "retour")
+            {
+                etapeSaisie = 0;
+                return;
+            }
+
+            foreach (var evenement in listeEvenements.Values)
+            {
+                if (evenement.Nom == nomEvenement)
+                {
+                    reserverBillet(clientConnecter, evenement);
+                    return;
+                }
+            }
+            // Si l'événement n'existe pas
+            Console.WriteLine("L'événement n'est pas valide /!\\");
+            etapeSaisie = 3; // Rester à l'étape de choix d'événement
         }
 
         public void reserverBillet(Client client, Evenement evenement)
         {
-            Console.WriteLine($"Vous êtes en train de reserver un billet pour {evenement.Nom} le {evenement.Date}");
-            Console.WriteLine("Quel type de place souhaitez vous ?");
+            Console.WriteLine($"Vous êtes en train de réserver un billet pour {evenement.Nom} le {evenement.Date}");
+            Console.WriteLine("Quel type de place souhaitez-vous ?");
             Console.WriteLine("0 - Standard\n1 - Gold\n2 - VIP");
-            string typePlace;
+            string typePlace = "";
             do
             {
                 string choixTypePlace = Console.ReadLine();
                 int choix;
-                if (int.TryParse(choixTypePlace, out choix) && choix <= 3 && choix >= 0)
+                if (int.TryParse(choixTypePlace, out choix) && choix <= 2 && choix >= 0)
                 {
                     switch (choix)
                     {
@@ -113,20 +186,20 @@ namespace ExerciceFinaleCs.Classes
                             typePlace = "VIP";
                             break;
                     }
+                    break; // Sortir de la boucle après avoir fait un choix valide
                 }
                 else
                 {
                     Console.WriteLine("Veuillez faire un choix valide.");
                 }
             } while (true);
+
             int numBillet = evenement.ListeBillet.Count + 1;
             Billet billetToAdd = new Billet(numBillet, client, evenement, typePlace);
             evenement.AjouterBillet(billetToAdd);
             client.AjouterBillet(billetToAdd);
-            Console.WriteLine($"Merci pour votre reservation {client.Prenom}, vous avez le billet numero {numBillet}");
-            return;
+            Console.WriteLine($"Merci pour votre réservation {client.Prenom}, vous avez le billet numéro {numBillet}");
+            etapeSaisie = 0; // Retour à l'introduction après la réservation
         }
-
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,15 +2,5 @@
 using ExerciceFinaleCs.Classes;
 
 
-//Clients
-
-Client client1 = new Client("Massé Laude", "Titouan", new Adresse("Jean Baptiste Lebas", "Provin"), 19, "0785297538");
-Console.WriteLine(client1.ToString());
-
-//Evenements
-Evenement event1 = new Evenement("Terrifier 3", new Lieu("Nationale", "Lille", 412),"19/10/2024");
-Console.WriteLine(event1.ToString());
-
-//Billets
-Billet billet1 = new Billet(1, client1, event1, "vip");
-Console.WriteLine(billet1.ToString());
+IHM iHM = new IHM();
+iHM.Start();
